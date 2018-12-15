f1abd0e81a99ddf5352cadf2b99755e124b403dd
Geniusjoe <843520313@qq.com>
2018-12-15 13:14:58 +0800

    add save file by time

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -4,11 +4,14 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import datetime
 
 class TutorialPipeline(object):
     def __init__(self):
-        self.f = open('../spiders/csgo.csv', 'a+',encoding='utf-8')
+        try:
+            self.f = open('../spiders/csgo_{}.csv'.format(str(datetime.date.today())), 'a+',encoding='utf-8')
+        except:
+            self.f=self.f = open('../spiders/csgo_{}.csv'.format(str(datetime.date.today())), 'w+',encoding='utf-8')
         self.fields = ["name", "selling_price", "selling_date"]  # define fields to use
         # self.f.write("{}\n".format(','.join(str(field)
         #                                     for field in self.fields)))  # write header
